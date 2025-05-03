ed82ae99987e13a28c4cbf3f942b107db8e4db12
Arun Alagusunthram <arunalag3003@gmail.com>
2025-05-03 19:22:00 +0800

    Set up Docker for End_To_End_ML_Pipeline project

---
+++ End_To_End_ML_Pipeline/Hello_World.py
@@ -0,0 +1,6 @@
+import time
+
+print("--- Script Starting ---") # Add this line
+print('Hello World')
+time.sleep(2) # Add this line (requires 'import time' at the top)
+print("--- Script Ending ---")   # Add this line
